a34c0ccee246062c072960c155a39125da93104a
VaraLakshmiCH25 <chavvakulavaralakshmi77@gmail.com>
2026-09-05 11:23:49 +0530

    Day 01 - Python input and output

---
+++ Day01/basics.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+city = input("Enter your city: ")
+technology = input("What technology are you learning? ")
+
+print("Hello", name)
+print("You are from", city)
+print("You are learning", technology)
